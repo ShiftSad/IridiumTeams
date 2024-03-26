6be2d5c148b91366831c831a265c0d0fcb01e873
ShiftSad <rosalimavictor@gmail.com>
2024-03-26 01:32:41 -0300

    Updated XSeries

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     implementation("org.jetbrains:annotations:24.1.0")
     implementation("com.j256.ormlite:ormlite-core:6.1")
     implementation("com.j256.ormlite:ormlite-jdbc:6.1")
-    implementation("com.github.ShiftSad:IridiumCore:049688efb5")
+    implementation("com.github.ShiftSad:IridiumCore:35097bc18d")
 
     // Other dependencies that are not required or already available at runtime
     compileOnly("org.projectlombok:lombok:1.18.32")
